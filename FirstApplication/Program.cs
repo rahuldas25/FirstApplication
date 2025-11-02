308c643a4eeb1240d48a3347f472383eaa35e1c7
SerializationV1 <rd09536@gmail.com>
2025-11-02 19:39:20 +0530

    Initial commit

---
+++ FirstApplication/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace add;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int number1,number2;
+        Console.Write("Enter the number 1: ");
+        number1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter the number 2: ");
+        number2 = Convert.ToInt32(Console.ReadLine());
+        int result = number1 + number2;
+        Console.WriteLine("Result: "+result);
+    }
+}
